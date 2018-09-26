28f28bfc992bbff13142420ed875283b6f4a356e
MelissaData <github@melissadata.com>
2018-09-25 18:25:35 -0700

    Changed Namespace

--- BusinessCoder-NET/Properties/Resources.Designer.cs
+++ BusinessCoder-NET/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BusinessCoder_NET_REST.Properties {
+namespace BusinessCoder_NET.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BusinessCoder_NET_REST.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BusinessCoder_NET_REST.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BusinessCoder_NET.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- BusinessCoder-NET/Properties/Settings.Designer.cs
+++ BusinessCoder-NET/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BusinessCoder_NET_REST.Properties {
+namespace BusinessCoder_NET.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
